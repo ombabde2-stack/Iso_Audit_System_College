104013060d27c8d026960d143cb996ca941ddefa
Om Babde <ombabde2@gmail.com>
2026-03-30 16:58:18 +0530

    feat: initialize backend with express server and basic structure

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Health check route
+//////***Creating an API endpoint */
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running successfully 🚀",
+  });
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
+export default app;
